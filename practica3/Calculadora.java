fc49457d887fad7c0bd47ef580ebf01ac251bd80
Konleywu912 <leymanwu912@gmail.com>
2025-06-16 15:15:33 -0600

    Update Calculadora.java

--- practica3/Calculadora.java
+++ practica3/Calculadora.java
@@ -29,6 +29,7 @@ public class Calculadora extends JFrame implements ActionListener {
         pantalla = new JTextField();
         pantalla.setFont(new Font("Arial", Font.BOLD, 24));
         pantalla.setEditable(false);
+        pantalla.setHorizontalAlignment(SwingConstants.RIGHT);
         add(pantalla, BorderLayout.NORTH);
 
         logica = new Logica();
